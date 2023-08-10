08906852e1a23af971b332d47fdb57bad69fd5d0
Mustafaozver <mustafa.ozver@hotmail.com>
2023-08-11 00:49:51 +0300

    11.8.2023

--- contracts/SNI.sol
+++ contracts/SNI.sol
@@ -10,34 +10,51 @@ import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 contract SNI is ERC721Upgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable{
 	using CountersUpgradeable for CountersUpgradeable.Counter;
 	CountersUpgradeable.Counter private _tokenIds;
-
+	
 	address admin;
-
+	
 	mapping (uint256 => TokenMeta) private _tokenMeta;
-
+	event NFTPurchased(address buyer, uint256 tokenId);
+	
 	struct TokenMeta {
 		uint256 id;
-
+	
 		string data;
 		address owner;
-
+	
 		uint256 price;
 		address token;
+		
+		bool sale;
+		bool rent;
 	}
-
+	
     function initialize() public initializer {
         OwnableUpgradeable.__Ownable_init();
         ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
         ERC721Upgradeable.__ERC721_init("SNI", "SNI");
         admin = msg.sender;
     }
-
+	
     function listItem(string memory _metadata, uint256 _price, address _token) external onlyOwner returns (uint256) {
 		_tokenIds.increment();
 		uint256 newItemId = _tokenIds.current();
 		_mint(admin, newItemId);
 		TokenMeta memory meta = TokenMeta(newItemId, _metadata, admin, _price, _token);
+		_tokenMeta[newItemId] = meta;
 		return newItemId;
     }
-
+    
+    function purchaseNFT(uint256 _tokenId) external {
+        require(ownerOf(_tokenId) != msg.sender, "You cannot purchase your own NFT.");
+        require(msg.sender != address(0), "Invalid buyer address.");
+        
+        IERC20 saleToken = IERC20(_tokenMeta[_tokenId].token);
+        saleToken.transferFrom(msg.sender, admin, _tokenMeta[_tokenId].price);
+        
+        _transfer(ownerOf(_tokenId), msg.sender, _tokenId);
+        _tokenMeta[_tokenId].owner = msg.sender;
+        
+    }
+	
 }
